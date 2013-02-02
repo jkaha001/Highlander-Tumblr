5318abf4d7fda105580c33c99aa1fa014901c151
animekraxe <yukibhuynh@gmail.com>
2013-02-01 22:16:40 -0800

    fixed settings

--- hlndr_tumblr/hlndr_tumblr/settings.py
+++ hlndr_tumblr/hlndr_tumblr/settings.py
@@ -1,10 +1,6 @@
 # Django settings for hlndr_tumblr project.
 import os
 
-AWS_SECRET_ACCESS_KEY = '+5mZMoNGYTso+RmK9H39MzihfUT8qeA3BZfe5fJ+'
-AWS_ACCESS_KEY_ID = 'AKIAIV3HV6NUFO3UD4XQ'
-AWS_STORAGE_BUCKET_NAME = 'highlander-tumblr-test-bucket'
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 DEBUG = True
