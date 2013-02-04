7ba7301aeafe737b4c7c8eab242f9434a6d23da4
animekraxe <yukibhuynh@gmail.com>
2013-02-04 02:44:08 -0800

    db clear

--- hlndr_tumblr/hlndr_tumblr/settings.py
+++ hlndr_tumblr/hlndr_tumblr/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 		'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 		'NAME': 'd8cmtbjk90pqvb',                      # Or path to database file if using sqlite3.
 		'USER': 'ailmervfaempjg',                      # Not used with sqlite3.
-		'PASSWORD': 'C7rekRJfdcHudLp999M6efXvn2',                  # Not used with sqlite3.
+		'PASSWORD': '',                  # Not used with sqlite3.
 		'HOST': 'ec2-54-243-243-217.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
 		'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
 	}
