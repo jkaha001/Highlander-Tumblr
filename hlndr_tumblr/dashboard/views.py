5f7fc78cebbc820895c998f65a654ec9a8ec0a76
animekraxe <yukibhuynh@gmail.com>
2013-02-08 00:08:26 -0800

    html changes and deletion

--- hlndr_tumblr/dashboard/views.py
+++ hlndr_tumblr/dashboard/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
@@ -13,4 +13,33 @@ def dashboard(request):
 
 	return render_to_response('dashboard/dashboard.html',
 							  context_instance=RequestContext(request))
-		
+
+def sort_posts_by_oldest(posts):
+	posts = sorted(posts, key=lambda post: post.post_date)
+	return posts
+
+def sort_posts_by_newest(posts):
+	posts = reversed(sorted(posts, key=lambda post: post.post_date))
+	return posts
+
+@login_required(login_url='/login/')
+def deletepost(request, post_type, post_id):
+	posts = get_post_list_by_author(request.user)
+	for post in posts:
+		if post.classname().lower() == post_type:
+			if post.id == int(post_id):
+				delete_post(post)
+				return HttpResponseRedirect('/' + request.user.username + '/posts/')
+	raise Http404
+
+@login_required(login_url='/login/')
+def viewposts(request, username):
+	posts = get_post_list_by_author(request.user)
+	
+	#posts = sort_posts_by_newest(posts)
+	#posts = sort_posts_by_oldest(posts)
+	posts = sort_posts_by_newest(posts)		
+	
+	return render_to_response('dashboard/viewposts.html',
+							  {'posts':posts, 'user':request.user},
+							  context_instance=RequestContext(request))			
