0cbf2a0b38e9550acc0c8c6d746c860efb00d2a1
animekraxe <yukibhuynh@gmail.com>
2013-02-12 15:28:39 -0800

    added sams code for following

---
+++ hlndr_tumblr/following/views.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404, render_to_response
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from users.models import UserProfile
+from blog.views import get_post_list_by_author, delete_post
+
+@login_required(login_url='/login/')
+def following(request):
+    if request.user.is_authenticated():
+        user = request.user
+    myself = get_object_or_404(User, username=user.username)
+    myprofile = get_object_or_404(UserProfile, user=myself)
+    return render_to_response('following/following.html', {'follower_list':myprofile.following.all()}, context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
+def follow(request, username):
+    if request.user.is_authenticated():
+        user = request.user.username
+    
+    #following
+    myself = get_object_or_404(User, username=request.user.username)
+    myprofile = get_object_or_404(UserProfile, user=myself)
+
+    other = get_object_or_404(User, username=username)
+    otherprofile = get_object_or_404(UserProfile, user=other)
+    if myprofile != otherprofile:
+        myprofile.following.add(otherprofile)
+        myprofile.save()
+
+    return render_to_response('following/following.html', {'follower_list':myprofile.following.all()}, context_instance=RequestContext(request))
+
--- hlndr_tumblr/users/models.py
+++ hlndr_tumblr/users/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
+	def __unicode__(self):
+        #prepended with 'self.' to resolve scoping issues within the shell
+		return self.user.username
+
   	GENDER_CHOICES = (
 		('u', 'Undisclosed'),
 		('m', 'Male'),
@@ -11,13 +15,14 @@ class UserProfile(models.Model):
 	)
     
 	user = models.OneToOneField(User)
-
+    
+    #following logic
+	following = models.ManyToManyField('UserProfile', related_name="list_following")
+    
 	# URL to avatar image hosted
 	avatar = models.URLField()
 	nickname = models.CharField(max_length=50, blank=True)
 	gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
 	birthday = models.DateField()
 	interests = models.CharField(max_length=500, blank=True)
-	
-	def __unicode__(self):
-		return user.username
+
